74618ed13388d7127182ca61834911e97e95ac7b
Hridesh Kumar <hridesh.k@edutreelearnings.com>
2023-09-25 15:26:55 +0530

    Todoapp

---
+++ src/components/Social.jsx
@@ -0,0 +1,20 @@
+export default function () {
+  return (
+    <header>
+      <div class="fixed h-full flex-grow flex flex-col justify-center items-center gap-2 after:bg-white after:h-20 after:w-10 after:absolute after:z-0">
+        <a
+          class="w-full pl-1 z-10"
+          href="https://github.com/Haridesh007/Todo_Reactjs"
+          target="_blank"
+        >
+          <img src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png" class="h-6 hover:scale-125 duration-300" />
+        </a>
+        <a
+       
+        >
+        
+        </a>
+      </div>
+    </header>
+  );
+}
